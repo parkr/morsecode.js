3815a353bc7d5a74f3b1b5e4fdee17ad9535bf33
Parker Moore <parkrmoore@gmail.com>
2013-07-03 23:42:33 +0200

    Wrapping in a function

--- morsecode.js
+++ morsecode.js
@@ -1,79 +1,87 @@
-var MorseCode = function() {
-    this.theCode = {
-        a: ". _",
-        b: "_ . . .",
-        c: "_ . _ .",
-        d: "_ . .",
-        e: ".",
-        f: ". . _ .",
-        g: "_ _ .",
-        h: ". . . .",
-        i: ". .",
-        j: ". _ _ _",
-        k: "_ . _",
-        l: ". _ . .",
-        m: "_ _",
-        n: "_ .",
-        o: "_ _ _",
-        p: ". _ _ .",
-        q: "_ _ . _",
-        r: ". _ .",
-        s: ". . .",
-        t: "_",
-        u: ". . _",
-        v: ". . . _",
-        w: ". _ _",
-        x: "_ . . _",
-        y: "_ . _ _",
-        z: "_ _ . .",
-        0: "_ _ _ _ _",
-        1: ". _ _ _ _",
-        2: ". . _ _ _",
-        3: ". . . _ _",
-        4: ". . . . _",
-        5: ". . . . .",
-        6: "_ . . . .",
-        7: "_ _ . . .",
-        8: "_ _ _ . .",
-        9: "_ _ _ _ ."
-    };
+// morsecode.js
+// Written with love by Parker Moore, github.com/parkr
+// MIT License
 
-    this.translate = function(to_translate) {
-        if (/[a-zA-Z0-9]+/.test(to_translate)) {
-            return this.translateToMorseCode(to_translate);
-        } else {
-            return this.translateToAlphanumeric(to_translate);
-        }
-    };
+(function() {
 
-    this.translateToMorseCode = function(to_translate) {
-        var characters = to_translate.toLowerCase().split("");
-        var morseCode  = this.theCode;
-        return characters.map(function(letter){
-            if(morseCode.hasOwnProperty(letter)) {
-                return morseCode[letter];
+    var MorseCode = function() {
+        this.theCode = {
+            a: ". _",
+            b: "_ . . .",
+            c: "_ . _ .",
+            d: "_ . .",
+            e: ".",
+            f: ". . _ .",
+            g: "_ _ .",
+            h: ". . . .",
+            i: ". .",
+            j: ". _ _ _",
+            k: "_ . _",
+            l: ". _ . .",
+            m: "_ _",
+            n: "_ .",
+            o: "_ _ _",
+            p: ". _ _ .",
+            q: "_ _ . _",
+            r: ". _ .",
+            s: ". . .",
+            t: "_",
+            u: ". . _",
+            v: ". . . _",
+            w: ". _ _",
+            x: "_ . . _",
+            y: "_ . _ _",
+            z: "_ _ . .",
+            0: "_ _ _ _ _",
+            1: ". _ _ _ _",
+            2: ". . _ _ _",
+            3: ". . . _ _",
+            4: ". . . . _",
+            5: ". . . . .",
+            6: "_ . . . .",
+            7: "_ _ . . .",
+            8: "_ _ _ . .",
+            9: "_ _ _ _ ."
+        };
+
+        this.translate = function(to_translate) {
+            if (/[a-zA-Z0-9]+/.test(to_translate)) {
+                return this.translateToMorseCode(to_translate);
             } else {
-                return letter;
+                return this.translateToAlphanumeric(to_translate);
             }
-        }).join(" ").replace(/\s+/g, " ");
+        };
+
+        this.translateToMorseCode = function(to_translate) {
+            var characters = to_translate.toLowerCase().split("");
+            var morseCode  = this.theCode;
+            return characters.map(function(letter){
+                if(morseCode.hasOwnProperty(letter)) {
+                    return morseCode[letter];
+                } else {
+                    return letter;
+                }
+            }).join(" ").replace(/\s+/g, " ");
+        };
+
+        this.translateToAlphanumeric = function(to_translate) {
+            return "NOT IMPLEMENTED ERROR ZOMG";
+        };
     };
 
-    this.translateToAlphanumeric = function(to_translate) {
-        return "NOT IMPLEMENTED ERROR ZOMG";
-    };
-};
-
-// This exporting of our function is lovingly stolen from Underscore.js.
-// Export the Underscore object for **Node.js**, with
-// backwards-compatibility for the old `require()` API. If we're in
-// the browser, add `MorseCode` as a global object via a string identifier,
-// for Closure Compiler "advanced" mode.
-if (typeof exports !== 'undefined') {
-    if (typeof module !== 'undefined' && module.exports) {
-        exports = module.exports = MorseCode;
+    // This exporting of our function is lovingly stolen from Underscore.js.
+    // Export the Underscore object for **Node.js**, with
+    // backwards-compatibility for the old `require()` API. If we're in
+    // the browser, add `MorseCode` as a global object via a string identifier,
+    // for Closure Compiler "advanced" mode.
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = MorseCode;
+        } else {
+            exports.MorseCode = MorseCode;
+        }
     } else {
-        exports.MorseCode = MorseCode;
+        window.MorseCode = MorseCode;
     }
-} else {
-    window.MorseCode = MorseCode;
-}
+
+}());
